efc45703e1504b732040c636b346353ef9c36f7b
Emma <emmajanefearn@aol.com>
2015-02-23 23:21:12 +0000

    Scrolling Menu Works
    Basic scrolling menu code is functional

--- app/src/main/java/teamprojectgroup3/codesyntaxguide/Menu.java
+++ app/src/main/java/teamprojectgroup3/codesyntaxguide/Menu.java
@@ -1,30 +1,13 @@
 package teamprojectgroup3.codesyntaxguide;
-
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
 
+public class Menu extends Activity
 
-public class Menu extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+{
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
