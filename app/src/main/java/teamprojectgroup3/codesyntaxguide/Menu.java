345da6f75f0427aafd100c57ddb8e3860d23124d
Emma <emmajanefearn@aol.com>
2015-02-24 00:07:22 +0000

    Button Listeners Implemented
    Button Listeners

--- app/src/main/java/teamprojectgroup3/codesyntaxguide/Menu.java
+++ app/src/main/java/teamprojectgroup3/codesyntaxguide/Menu.java
@@ -1,13 +1,114 @@
 package teamprojectgroup3.codesyntaxguide;
-import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class Menu extends Activity
+public class Menu extends ActionBarActivity {
 
-{
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Declare Buttons
+        Button button = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button button10 = (Button) findViewById(R.id.button10);
+        Button button11 = (Button) findViewById(R.id.button11);
+        Button button12 = (Button) findViewById(R.id.button12);
+        Button button13 = (Button) findViewById(R.id.button13);
+        Button button14 = (Button) findViewById(R.id.button14);
+
+        //On click of a buttons listener
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button1:
+                        Intent intent = new Intent(Menu.this, Overview.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.button2:
+                        Intent intent2 = new Intent(Menu.this, Recommendations.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.button3:
+                        Intent intent3 = new Intent(Menu.this, Outgoings.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.button4:
+                        Intent intent4 = new Intent(Menu.this, Income.class);
+                        startActivity(intent4);
+                        break;
+                    case R.id.button5:
+                        Intent intent5 = new Intent(Menu.this, Settings.class);
+                        startActivity(intent5);
+                        break;
+                    case R.id.button6:
+                        Intent intent6 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent6);
+                        break;
+                    case R.id.button7:
+                        Intent intent7 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent7);
+                        break;
+                    case R.id.button8:
+                        Intent intent8 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent8);
+                        break;
+                    case R.id.button9:
+                        Intent intent9 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent9);
+                        break;
+                    case R.id.button10:
+                        Intent intent10 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent10);
+                        break;
+                    case R.id.button11:
+                        Intent intent11 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent11);
+                        break;
+                    case R.id.button12:
+                        Intent intent12 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent12);
+                        break;
+                    case R.id.button13:
+                        Intent intent13 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent13);
+                        break;
+                    case R.id.button14:
+                        Intent intent14 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent14);
+                        break;
+                }
+            }
+        };
+
+        //Set the listener
+        button.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+        button8.setOnClickListener(listener);
+        button9.setOnClickListener(listener);
+        button10.setOnClickListener(listener);
+        button11.setOnClickListener(listener);
+        button12.setOnClickListener(listener);
+        button13.setOnClickListener(listener);
+        button14.setOnClickListener(listener);
     }
 }
 
